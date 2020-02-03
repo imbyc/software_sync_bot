2143c769ad3373bd38bddc3959b54102b508abd5
byc <23715018@qq.com>
2020-02-03 19:41:17 +0800

    测试蓝奏云登录

---
+++ lanzou.py
@@ -0,0 +1,23 @@
+import os
+from lanzou.api import LanZouCloud
+
+lanzou_username = os.getenv('LANZOU_USERNAME')
+lanzou_password = os.getenv('LANZOU_PASSWORD')
+
+lzy = LanZouCloud()
+code = lzy.login(lanzou_username, lanzou_password)
+if code == LanZouCloud.SUCCESS:
+    print('登录成功')
+else if code == LanZouCloud.FAILED
+    print('登录失败')
+else if code == LanZouCloud.NETWORK_ERROR
+    print('网络异常')
+
+
+
+# 设置rar路径
+code = lzy.set_rar_tool('/usr/bin/rar')
+if code == LanZouCloud.SUCCESS:
+    print('RAR路径设置成功')
+else if code == LanZouCloud.ZIP_ERROR
+    print('RAR路径设置失败')
